56b6ba6637c6fd5e97841ccb8c94f80285d3cc88
Andrey-Ionel <a-ionel@ukr.net>
2023-05-29 16:18:11 +0300

    add project

---
+++ App.tsx
@@ -0,0 +1,110 @@
+import React, {useEffect} from 'react';
+import {
+  PermissionsAndroid,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  View,
+} from 'react-native';
+
+import Geolocation from 'react-native-geolocation-service';
+
+const App: () => React.ReactNode = () => {
+  const requestLocationPermission = async (): Promise<boolean> => {
+    try {
+      Geolocation.requestAuthorization('whenInUse').then(status => {
+        if (status === 'granted') {
+          return true;
+        } else {
+          return false;
+        }
+      });
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  };
+
+  const requestLocationPermissionAndroid = async (): Promise<boolean> => {
+    try {
+      await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Geolocation Permission',
+          message: 'Can we access your location?',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      )
+        .then(res => {
+          if (res === 'granted') {
+            return true;
+          } else {
+            console.log('You cannot use Geolocation');
+            return false;
+          }
+        })
+        .catch(e => {
+          console.log(e);
+          return false;
+        });
+    } catch (err) {
+      return false;
+    }
+  };
+
+  useEffect(() => {
+    const isAllowed =
+      Platform.OS !== 'android'
+        ? requestLocationPermission().catch(error => console.log(error))
+        : requestLocationPermissionAndroid().catch(error => console.log(error));
+
+    if (!!isAllowed) {
+      Geolocation.getCurrentPosition(
+        position => {
+          console.log(50, 'zxc', 'position', position);
+        },
+        error => {
+          // См. таблицы кодов ошибок выше.
+          if (error.code === 1) {
+            // Platform.OS !== 'android'
+            //   ? requestLocationPermission().catch(error => console.log(error))
+            //   : requestLocationPermissionAndroid().catch(error =>
+            //       console.log(error),
+            //     );
+          }
+          console.log(error.code, error.message);
+        },
+        {
+          enableHighAccuracy: false,
+          timeout: 10000,
+          maximumAge: 100000,
+        },
+      );
+    } else {
+      return;
+    }
+  }, []);
+
+  return (
+    <SafeAreaView style={{backgroundColor: 'red'}}>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={{backgroundColor: 'red'}}
+        >
+        <View style={{backgroundColor: 'white'}}>
+            <Text>{'HELLO'}</Text>
+        </View>
+        <View style={{backgroundColor: 'white'}}>
+            <Text>{'HELLO'}</Text>
+        </View>
+        <View style={{backgroundColor: 'white'}}>
+            <Text>{'HELLO'}</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const webpack = require('webpack');
+const rootDir = path.join(__dirname, '../App');
+const webpackEnv = process.env.NODE_ENV || 'development';
+
+module.exports = {
+  mode: webpackEnv,
+  entry: {
+    app: path.resolve(rootDir, 'index.js'),
+  },
+  output: {
+    path: path.resolve(rootDir, 'dist'),
+    filename: 'bundle.js',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts|jsx|js|mjs)$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: [
+      '.web.tsx',
+      '.web.ts',
+      '.tsx',
+      '.ts',
+      '.web.jsx',
+      '.web.js',
+      '.jsx',
+      '.js',
+    ], // read files in fillowing order
+    alias: Object.assign({
+      'react-native$': 'react-native-web',
+    }),
+  },
+};
